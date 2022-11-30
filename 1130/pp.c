0ee2af28e49deafb115211dcbced493e6c6a9375
suhyun6363 <thebbbb1206@gmail.com>
2022-11-30 16:12:57 +0900

    1130 sec

--- 1130/pp.c
+++ 1130/pp.c
@@ -5,13 +5,13 @@ int main(void) {
 	int* b[3];
 	int **p;
 
-	p = b;
-
 	for(int i = 0; i<3; i++) {
 		b[i] = &a[i];
 	}
+	
+	p = b;
 	for(int i = 0; i<3; i++) {
-		**p += 1;
+		(**p) += 1;
 		p++;
 	}
 	p = b;
